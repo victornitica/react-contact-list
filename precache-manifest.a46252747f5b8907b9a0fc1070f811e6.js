5ad2793acaa392bc0c46161b534fd8f5f1103c5e
victornitica <victornitica@gmail.com>
2020-09-01 14:39:13 +0300

    Updates

---
+++ precache-manifest.a46252747f5b8907b9a0fc1070f811e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4662a25d1058caa21feb519193110e37",
+    "url": "/react-contact-list/index.html"
+  },
+  {
+    "revision": "873dc35ab4a2340d86c3",
+    "url": "/react-contact-list/static/css/main.0a999c82.chunk.css"
+  },
+  {
+    "revision": "0ba148db66beacef465d",
+    "url": "/react-contact-list/static/js/2.022bd6ff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-contact-list/static/js/2.022bd6ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "873dc35ab4a2340d86c3",
+    "url": "/react-contact-list/static/js/main.d2d36ccf.chunk.js"
+  },
+  {
+    "revision": "ae7678212b1f1004d5e9",
+    "url": "/react-contact-list/static/js/runtime-main.8cbf87cf.js"
+  }
+]);
